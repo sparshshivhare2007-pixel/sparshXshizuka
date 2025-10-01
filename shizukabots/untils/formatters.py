b54c3db8a4dd92c35400eb327797ab58ad263208
⏤͟͞ 𝙎𝙋𝘼𝙍𝙎𝙃 𝘽𝘼𝙉𝙄𝙔𝘼 <sparshshivhare2007@gmail.com>
2025-10-01 12:28:35 +0530

    𝑺𝒉𝒊𝒛𝒖𝒌𝒂𝒃𝒐𝒕𝒔

--- shizukabots/untils/formatters.py
+++ shizukabots/untils/formatters.py
@@ -80,7 +80,7 @@ async def alpha_to_int(user_id_alphabet: str) -> int:
 
 
 def time_to_seconds(time):
-    stringt = str(time)
+    stringt = str(time) 
     return sum(int(x) * 60**i for i, x in enumerate(reversed(stringt.split(":"))))
 
 
