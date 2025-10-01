e7fb65a85272716eb7c89e653db0430122577292
⏤͟͞ 𝙎𝙋𝘼𝙍𝙎𝙃 𝘽𝘼𝙉𝙄𝙔𝘼 <sparshshivhare2007@gmail.com>
2025-10-01 06:44:06 +0530

    𝑺𝒉𝒊𝒛𝒖𝒌𝒂𝒃𝒐𝒕𝒔

--- shizukabots/logging.py
+++ shizukabots/logging.py
@@ -1,3 +1,25 @@
+# Copyright (c) 2025 sparsh baniya <baniya>
+# Location: agra, up 
+#
+# All rights reserved.
+#
+# This code is the intellectual property of sparsh baniya.
+# You are not allowed to copy, modify, redistribute, or use this
+# code for commercial or personal projects without explicit permission.
+#
+# Allowed:
+# - Forking for personal learning
+# - Submitting improvements via pull requests
+#
+# Not Allowed:
+# - Claiming this code as your own
+# - Re-uploading without credit or permission
+# - Selling or using commercially
+#
+# Contact for permissions:
+# Email: sparshshivhare20007@gmail.com
+
+
 import logging
 
 logging.basicConfig(
