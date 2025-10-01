a5bb6ddfb79a71608d458d41f82538f342d74c9b
⏤͟͞ 𝙎𝙋𝘼𝙍𝙎𝙃 𝘽𝘼𝙉𝙄𝙔𝘼 <sparshshivhare2007@gmail.com>
2025-10-01 12:30:54 +0530

    𝑺𝒉𝒊𝒛𝒖𝒌𝒂𝒃𝒐𝒕𝒔

--- shizukabots/untils/pastebin.py
+++ shizukabots/untils/pastebin.py
@@ -35,7 +35,7 @@ async def post(url: str, *args, **kwargs):
         return data
 
 
-async def sparshBin(text):
+async def sparshBin(text): 
     resp = await post(f"{BASE}api/v2/paste", data=text)
     if not resp["success"]:
         return
